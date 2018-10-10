cf5f30c6c7a0312e1a719948f02dd8851fc1e111
cnfc <stdashkov@mail.ru>
2018-10-10 14:01:07 +0300

    add Stripes & text on firsst page

---
+++ src/Components/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Featured from './featured';
+
+
+
+const Home = () => {
+  return (
+    <div className="bck_blue">
+      <Featured/>
+    </div>
+  );
+};
+
+export default Home;
