f45a62b8048ef8af8a2f6d7e510e6eb50eefe6b2
cnfc <stdashkov@mail.ru>
2018-10-10 09:53:27 +0300

    add Footer with logo

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button'
 
 import {Link} from 'react-router-dom';
+import { CityLogo } from '../ui/Icons';
+
 
 const Header = () => {
   return (
@@ -17,9 +19,16 @@ const Header = () => {
       }}
       >
 
-    <Toolbar styles={{display: 'flex'}}>
-      <div styles={{flesGrow: 1}}>
-        <div className='header_logo'>LOGO</div>
+    <Toolbar style={{display: 'flex'}}>
+      <div style={{flexGrow: 1}}>
+        <div className='header_logo'>
+          <CityLogo
+            link={true}
+            linkTo="/"
+            width='70px'
+            height="70px"
+            />
+        </div>
       </div>
 
       <Link to="/the_team">
