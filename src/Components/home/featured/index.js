0de9152514f5e7f0eb17a6c4e1b60be062f0da22
cnfc <stdashkov@mail.ru>
2018-10-10 17:17:39 +0300

    Add player`s logo

--- src/Components/home/featured/index.js
+++ src/Components/home/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stripes from './Stripes';
+import Text from './Text';
 
 
 
@@ -7,6 +8,7 @@ const Featured = () => {
   return (
     <div className="featured_wrapper">
       <Stripes/>
+      <Text/>
     </div>
   );
 };
