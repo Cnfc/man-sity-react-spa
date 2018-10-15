5fed57d42eacfcb7d50276d904bcf502c0b3ce31
cnfc <stdashkov@mail.ru>
2018-10-15 16:31:49 +0300

    add email button logic & del components

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import FormField from '../../ui/formField';
+import {validate} from '../../ui/misc';
 
 
 class Enroll extends Component {
@@ -26,6 +27,23 @@ class Enroll extends Component {
     }
   }
 
+  updateForm (element) {
+    const newFormdata = {...this.state.formdata}
+    const newElement = {...newFormdata[element.id]}
+
+    newElement.value = element.event.target.value;
+
+    let validData = validate(newElement);
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+
+    newFormdata[element.id] = newElement;
+    this.setState({
+      formdata: newFormdata
+    })
+  }
+
+
   submitForm () {
 
   }
@@ -42,6 +60,9 @@ class Enroll extends Component {
               <FormField
                 id={'email'}
                 formdata={this.state.formdata.email}
+                change={(element) => this.updateForm(element)
+
+                }
               />
             </div>
           </form>
--- src/Components/home/promotion/index.js
+++ src/Components/home/promotion/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PromotionAnimation from './Animation';
 import Enroll from './Enroll';
 
