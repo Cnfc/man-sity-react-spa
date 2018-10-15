aebfb5d84d43f56a9b852a356054d7faf3625bec
cnfc <stdashkov@mail.ru>
2018-10-15 11:31:36 +0300

    matches block*6 part

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {firebaseMatches} from '../../../firebase';
 import {firebaseLooper, reverseArray} from '../../ui/misc';
 
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 class Blocks extends Component {
 
@@ -19,10 +21,17 @@ class Blocks extends Component {
     })
   }
 
-  showMatches = () => (
-    <div>
-      match
-    </div>
+  showMatches = (matches) => (
+    matches ? matches.map((match)=> (
+      <Slide bottom key={match.id}>
+        <div className='item'>
+          <div className='wrapper'>
+            <MatchesBlock match={match}/>
+          </div>
+        </div>
+      </Slide>
+    ))
+    :null
   )
 
 
