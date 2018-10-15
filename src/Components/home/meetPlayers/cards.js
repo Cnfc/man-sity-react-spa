06d0f97d91e0c596e6f44ad2ca310a2ac47fe88e
cnfc <stdashkov@mail.ru>
2018-10-15 13:47:41 +0300

    complete player page(Otamendi)

--- src/Components/home/meetPlayers/cards.js
+++ src/Components/home/meetPlayers/cards.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {easePolyOut} from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
+import PlayerCard from '../../ui/playerCard';
+
 
 class HomeCards extends Component {
 
@@ -52,7 +54,12 @@ class HomeCards extends Component {
                 bottom
               }}
             >
-              div
+              <PlayerCard
+                name="Nicolas"
+                number="30"
+                lastname="Otamendi"
+                bck={Otamendi}
+              />
             </div>
           )
         }}
