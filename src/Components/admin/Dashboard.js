7d75c5c6d4aaf08cee688c4abf529dba15a36fb1
cnfc <stdashkov@mail.ru>
2018-10-17 13:36:11 +0300

    add left-menu and log out()

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdminLayout from '../../Hoc/AdminLayout';
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <h1>Dashboard Component</h1>
+      </div>
+
+    </AdminLayout>
+  );
+};
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,16 @@ import {Switch, Route} from  'react-router-dom';
 
 import Home from './Components/home';
 import SighIn from './Components/signin';
+import Dashboard from './Components/admin/Dashboard';
+
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+
+
+        <Route exact component={Dashboard} path='/dashboard'/>
         <Route exact component={SighIn} path='/sign_in'/>
         <Route exact component={Home} path='/'/>
 
