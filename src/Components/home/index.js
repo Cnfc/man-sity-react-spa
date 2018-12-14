2fae320cc150c4abd076cbecfc6faa09a5831a5f
Cnfc <stdashkov@mail.ru>
2018-12-14 18:29:42 +0300

    add Stripes

---
+++ src/Components/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Featured from './featured/';
+
+
+const Home = (props) => (
+    <div className="bck_blue">
+        <Featured/>
+    </div>
+);
+
+export default Home;
