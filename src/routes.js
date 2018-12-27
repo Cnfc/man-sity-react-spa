977d35bdf10d0d90cf9e9810449a1e1f91a872df
Cnfc <stdashkov@mail.ru>
2018-12-27 19:00:53 +0300

    privatRoute

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route }  from 'react-router-dom';
 
+import PrivateRoutes from './Components/authRoutes/privateRoutes';
+import PublicRoutes from './Components/authRoutes/publicRoutes';
+
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
 
 const Routes = (props) => {
+
+
   return(
     <Layout>
         <Switch>
-            <Route component={Dashboard} path="/dashboard" />
-            <Route exact component={Home} path="/"/>
+            <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
+
             <Route component={SignIn} path="/sign_in" />
+
+            <Route component={Home} path="/"/>
             
         </Switch>
     </Layout>
