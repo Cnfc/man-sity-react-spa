9448631b90dd02e87ec94126fb523f77a560cc93
Cnfc <stdashkov@mail.ru>
2019-01-14 17:26:50 +0300

    add CircularProgress

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import PublicRoutes from './Components/authRoutes/publicRoutes';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
+import AdminMatches from './Components/admin/matches';
 
 const Routes = (props) => {
 
@@ -15,14 +16,10 @@ const Routes = (props) => {
   return(
     <Layout>
         <Switch>
+            <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches} />
             <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
-
             <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
-            <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
-
-
-            
-            
+            <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />           
         </Switch>
     </Layout>
   )
