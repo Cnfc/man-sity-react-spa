116f89967c1dd3ab4d9d46ece17bf6e59228f82c
Cnfc <stdashkov@mail.ru>
2019-01-15 18:37:12 +0300

    add Tablesell in results

--- src/Components/admin/matches/index.js
+++ src/Components/admin/matches/index.js
@@ -35,12 +35,56 @@ class AdminMatches extends Component {
     render() {
         return (
             <AdminLayout>
-                <div className='admin_progress'>
-                    {
-                        this.state.isloading ? 
-                            <CircularProgress thickness={7} style={{color:'#98c5e9'}}/>
-                        : ''
-                    }
+                <div className=''>
+                    <Papar>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Date</TableCell>
+                                    <TableCell>Match</TableCell>
+                                    <TableCell>Result</TableCell>
+                                    <TableCell>Final</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                { this.state.matches ? 
+                                    this.state.matches.map((match, i) => (
+                                        <TableRow key={i}>
+                                            <TableCell>
+                                                {match.date}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Link to={`/admin_matches/edit_match/${match.id}`}>
+                                                    {match.away} <strong>-</strong> {match.local}
+                                                </Link>
+                                            </TableCell>
+                                            
+                                            <TableCell>
+                                                {match.resultAway} <strong>-</strong> {match.resultLocal}
+                                            </TableCell>
+                                            <TableCell>
+                                                { match.final === 'Yes' ? 
+                                                    <span className="matches_tag_red">Final</span>
+                                                    : 
+                                                    <span className="matches_tag_green">Not played yet</span>
+                                                }
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                    
+                                    : null
+                                }
+                            </TableBody>
+                        </Table>
+                    </Papar>
+                
+                    <div className='admin_progress'>
+                        {
+                            this.state.isloading ? 
+                                <CircularProgress thickness={7} style={{color:'#98c5e9'}}/>
+                            : ''
+                        }
+                    </div>
                 </div>
             </AdminLayout>
         );
